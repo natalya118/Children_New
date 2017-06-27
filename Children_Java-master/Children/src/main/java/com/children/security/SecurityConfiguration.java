a70122edd438cfddb74f71413b5ae61e8a23ea81
natalya118 <nutblog88@gmail.com>
2017-06-27 10:03:19 +0300

    skipped admin

--- Children_Java-master/Children/src/main/java/com/children/security/SecurityConfiguration.java
+++ Children_Java-master/Children/src/main/java/com/children/security/SecurityConfiguration.java
@@ -36,7 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/admin")
+		http.authorizeRequests().antMatchers("/adminnn")
 				.access("hasRole('ADMINMAIN')")
 				.antMatchers("/dlist")
 				.access("hasRole('USER') or hasRole('ADMIN')")
